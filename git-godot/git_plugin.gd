2f29439a8dbc7463ee4c767c463b912a4c4a72f4
William Tumeo <tumeowilliam@gmail.com>
2016-10-17 05:28:05 -0200

    Fix path name + remove on exit tree

--- git-godot/git_plugin.gd
+++ git-godot/git_plugin.gd
@@ -5,13 +5,13 @@ var git_control = null
 var button = null
 
 func _enter_tree():
-	git_control = preload("res://addons/git/git_control.tscn").instance()
+	git_control = preload("res://addons/git-godot/git_control.tscn").instance()
 	button = add_control_to_bottom_panel( git_control, "GIT" )
 	
 	button.connect("pressed", self, "_on_button_pressed")
 
 func _exit_tree():
-		git_control.free()
+	remove_control_from_bottom_panel(git_control)
 
 func _on_button_pressed():
 	var git = OS.execute("git", ["status"], false)
